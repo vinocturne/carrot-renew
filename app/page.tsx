f88dbd42b9285c83e1e27df28f635eb0630d2ae7
신건희 <godgh@emforce.co.kr>
2024-06-17 16:11:10 +0900

    form modifier

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 export default function Home() {
   return (
-    <main className="bg-gray-300 h-screen p-5 flex items-center justify-center ">
-      <div className="bg-white max-w-screen-sm w-full shadow-lg p-5 rounded-2xl flex flex-col gap-2">
+    <main className="bg-gray-300 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen p-5 flex items-center justify-center ">
+      <div className="bg-white max-w-screen-sm w-full shadow-lg p-5 rounded-2xl flex flex-col md:flex-row gap-2">
         <input
-          className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
-          type="text"
-          placeholder="Search here"
+          className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring:bg-red-600 peer"
+          type="email"
+          required
+          placeholder="Email Address"
         />
-        <button className="bg-black text-white bg-opacity-50 py-2 rounded-full active:scale-90 outline-none focus:scale-90 transition-transform font-medium">
-          Search
+        <span className="text-red-500 font-medium hidden peer-invalid:block">Email is required</span>
+        <button className="text-white bg-opacity-50 py-2 rounded-full active:scale-90 outline-none focus:scale-90 transition-transform font-medium md:px-10 bg-black peer-required:bg-green-400">
+          Log in
         </button>
       </div>
     </main>
