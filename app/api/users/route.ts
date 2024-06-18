777a639e9588fbf5305a5f8acc3ae83d2afe7892
신건희 <godgh@emforce.co.kr>
2024-06-18 17:33:00 +0900

    api route

---
+++ app/api/users/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  return Response.json({
+    ok: true,
+  });
+}
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+
+  return Response.json(data);
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,19 @@
+'use client';
+
 import FormButton from '@/components/form-btn';
 import FormInput from '@/components/form-input';
 import SocialLogin from '@/components/social-login';
 export default function Login() {
+  const onClick = async () => {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: 'hago',
+        password: 1234,
+      }),
+    });
+    console.log(await response.json());
+  };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -11,8 +23,10 @@ export default function Login() {
       <form className="flex flex-col gap-3">
         <FormInput type="email" placeholder="Email" required errors={[]} />
         <FormInput type="password" placeholder="Password" required errors={[]} />
-        <FormButton loading={false} text="Log in" />
       </form>
+      <span onClick={onClick}>
+        <FormButton loading={false} text="Log in" />
+      </span>
       <SocialLogin />
     </div>
   );
