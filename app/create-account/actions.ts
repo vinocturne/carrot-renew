33002e82ced2be61ffedfbdd894baa5a3c6bea1c
신건희 <godgh@emforce.co.kr>
2024-07-08 15:34:35 +0900

    join

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,5 +1,10 @@
 'use server';
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from '@/lib/constants';
+import db from '@/lib/db';
+import bcrypt from 'bcrypt';
+import { getIronSession } from 'iron-session';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
 const checkUsername = (username: string) => {
@@ -8,6 +13,30 @@ const checkUsername = (username: string) => {
 
 const checkPasswords = ({ password, confirm_password }: { password: string; confirm_password: string }) => {
   return password === confirm_password;
+};
+
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email: email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
 };
 
 const formSchema = z
@@ -19,11 +48,15 @@ const formSchema = z
       })
       .toLowerCase()
       .trim()
-      .transform(username => `🔥 ${username}`)
-      .refine(username => checkUsername(username), 'no potatos allowed'),
-    email: z.string().email().trim().toLowerCase(),
-    password: z.string().min(PASSWORD_MIN_LENGTH).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
-    confirm_password: z.string().min(10),
+      // .transform(username => `${username}`)
+      .refine(checkUsername, 'no potatos allowed')
+      .refine(checkUniqueUsername, 'This username is already taken!'),
+    email: z.string().email().trim().toLowerCase().refine(checkUniqueEmail, 'This email is already taken!'),
+    password: z.string(),
+    // .min(PASSWORD_MIN_LENGTH),
+    // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+    confirm_password: z.string(),
+    // .min(PASSWORD_MIN_LENGTH),
   })
   .refine(({ password, confirm_password }) => checkPasswords({ password, confirm_password }), {
     message: 'Both passwords should be same',
@@ -31,17 +64,35 @@ const formSchema = z
   });
 
 export async function createAccount(prevState: any, formData: FormData) {
+  console.log(cookies());
   const data = {
     username: formData.get('username'),
     email: formData.get('email'),
     password: formData.get('password'),
     confirm_password: formData.get('confirm_password'),
   };
-  const result = formSchema.safeParse(data);
-  console.log(result);
+  const result = await formSchema.spa(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    const cookie = await getIronSession(cookies(), {
+      cookieName: 'delicious-karrot',
+      password: process.env.COOKIE_PASSWORD!,
+    });
+    //@ts-ignore
+    cookie.id = user.id;
+    await cookie.save();
+    redirect('/profile');
   }
 }
